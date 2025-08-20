a7e1311af9a40b8bd31835ad3ea9f0ca1b01da78
Vishwaraj Korimath <165015812+v-king19@users.noreply.github.com>
2025-08-20 20:16:47 +0530

    Add files via upload

---
+++ backend/target/classes/data.sql
@@ -0,0 +1,3 @@
+insert into items(title, details, status, created_at, updated_at) values
+('First Task','Set up the project','OPEN', now(), now()),
+('Second Task','Build UI','IN_PROGRESS', now(), now());
